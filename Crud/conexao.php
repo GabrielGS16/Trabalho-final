1f210d8d47c7696f5aebeb454bdcf43900bd1d2e
GabrielGS16 <gabrielgirao760@gmail.com>
2025-06-11 14:11:04 -0300

    All Files Updated
    inicio do css, fiz o formulário e to começando a fazer a conexão

---
+++ Crud/conexao.php
@@ -0,0 +1,11 @@
+<?php
+$usuario ="root";
+$senha="";
+$dbname="cadastro";
+$host ="localhost";
+
+try{
+    $pdo = new PDO("mysql;dbname = $dbname;host= $host,$usuario)
+ }  
+
+?>
---
+++ Crud/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Cadastre-se</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body >
+    <div>
+     <h1>Cadastre-se</h1>
+    </div>
+    <div id="formulario">
+    <form action="">
+        <p>
+            <label class="label" for="">Nome: </label><br>
+            <input type="text">
+        </p>
+        <p>
+            <label class="label" for="">E-mail: </label><br>
+            <input type="text" name="email">
+        </p>
+        <p>
+            <label class="label" for="">Senha: </label><br>
+            <input type="passoword" name="senha">
+        </p>
+    </form>
+    </div>
+</body>
+</html>
